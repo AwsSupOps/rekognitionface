27d9fdc1374bcb6dbd071e7431d5a48fcfe27490
AwsSupOps <agbalouama@gmail.com>
2024-02-16 16:07:09 +0000

    5 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,72 @@
 import React from 'react';
 import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';
-import { ToggleButtonGroup, ToggleButton ,ThemeProvider } from '@aws-amplify/ui-react';
+import { ToggleButtonGroup, ToggleButton ,ThemeProvider, Loader } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import awsexports from './aws-exports';
 Amplify.configure(awsexports);
 
-const dictionary = {
-  // use default strings for english
-  en: null,
-  es: {
-    photosensitivyWarningHeadingText: 'Advertencia de fotosensibilidad',
-    photosensitivyWarningBodyText:
-      'Esta verificación muestra luces de colores. Tenga cuidado si es fotosensible.',
-    goodFitCaptionText: 'Buen ajuste',
-    tooFarCaptionText: 'Demasiado lejos',
-    hintCenterFaceText: 'Centra tu cara',
-    startScreenBeginCheckText: 'Comenzar a verificar',
-  },
-};
 
- function Customizationi18n() {
-  const [language, setLanguage] = React.useState<string>('en');
-  const [showLiveness, setShowLiveness] = React.useState(false);
+
+ function LivenessQuickStartReact() {
+  const [loading, setLoading] = React.useState(true);
+  const [createLivenessApiData, setCreateLivenessApiData] =
+    React.useState(null);
+
   React.useEffect(() => {
-    setShowLiveness(true);
+    const fetchCreateLiveness = async () => {
+      /*
+       * This should be replaced with a real call to your own backend API
+       */
+      await new Promise((r) => setTimeout(r, 2000));
+      const mockResponse = { sessionId: 'mockSessionId' };
+      const data = mockResponse;
+
+      setCreateLivenessApiData(data);
+      setLoading(false);
+    };
+
+    fetchCreateLiveness();
   }, []);
+
+  const handleAnalysisComplete = async () => {
+    /*
+     * This should be replaced with a real call to your own backend API
+     */
+    const response = await fetch(
+      `/api/get?sessionId=${createLivenessApiData.sessionId}`
+    );
+    const data = await response.json();
+
+    /*
+     * Note: The isLive flag is not returned from the GetFaceLivenessSession API
+     * This should be returned from your backend based on the score that you
+     * get in response. Based on the return value of your API you can determine what to render next.
+     * Any next steps from an authorization perspective should happen in your backend and you should not rely
+     * on this value for any auth related decisions.
+     */
+    if (data.isLive) {
+      console.log('User is live');
+    } else {
+      console.log('User is not live');
+    }
+  };
+
   return (
     <ThemeProvider>
-      <ToggleButtonGroup
-        value={language}
-        isExclusive
-        onChange={(value) => setLanguage(value)}
-      >
-        <ToggleButton value="en">En</ToggleButton>
-        <ToggleButton value="es">Es</ToggleButton>
-      </ToggleButtonGroup>
-      {showLiveness && (
+      {loading ? (
+        <Loader />
+      ) : (
         <FaceLivenessDetector
-          sessionId={'sessionId'}
+          sessionId={createLivenessApiData.sessionId}
           region="us-east-1"
-          onAnalysisComplete={async () => {}}
-          displayText={dictionary[language]}
+          onAnalysisComplete={handleAnalysisComplete}
+          onError={(error) => {
+            console.error(error);
+          }}
         />
       )}
     </ThemeProvider>
   );
 }
-
-export default Customizationi18n
+export default LivenessQuickStartReact
