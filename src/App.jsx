79a075ba9397a7cadd1722442d6e1a340c834578
AwsSupOps <agbalouama@gmail.com>
2024-02-16 15:59:19 +0000

    4 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';
+import { ToggleButtonGroup, ToggleButton ,ThemeProvider } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import awsexports from './aws-exports';
+Amplify.configure(awsexports);
 
-function App() {
-  const [count, setCount] = useState(0)
+const dictionary = {
+  // use default strings for english
+  en: null,
+  es: {
+    photosensitivyWarningHeadingText: 'Advertencia de fotosensibilidad',
+    photosensitivyWarningBodyText:
+      'Esta verificación muestra luces de colores. Tenga cuidado si es fotosensible.',
+    goodFitCaptionText: 'Buen ajuste',
+    tooFarCaptionText: 'Demasiado lejos',
+    hintCenterFaceText: 'Centra tu cara',
+    startScreenBeginCheckText: 'Comenzar a verificar',
+  },
+};
 
+ function Customizationi18n() {
+  const [language, setLanguage] = React.useState<string>('en');
+  const [showLiveness, setShowLiveness] = React.useState(false);
+  React.useEffect(() => {
+    setShowLiveness(true);
+  }, []);
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider>
+      <ToggleButtonGroup
+        value={language}
+        isExclusive
+        onChange={(value) => setLanguage(value)}
+      >
+        <ToggleButton value="en">En</ToggleButton>
+        <ToggleButton value="es">Es</ToggleButton>
+      </ToggleButtonGroup>
+      {showLiveness && (
+        <FaceLivenessDetector
+          sessionId={'sessionId'}
+          region="us-east-1"
+          onAnalysisComplete={async () => {}}
+          displayText={dictionary[language]}
+        />
+      )}
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default Customizationi18n
